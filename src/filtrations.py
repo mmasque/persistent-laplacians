007909dba0e1a6e365d26e181501dbe149305464
Marcel Masque Salgado <marcel.masque.salgado@prodrive-technologies.com>
2025-04-30 00:30:52 +0200

    temporary changes, looking into computation failure as datapts increase

--- src/filtrations.py
+++ src/filtrations.py
@@ -3,14 +3,16 @@ from collections import defaultdict
 
 import bisect
 
+
 def simplices_by_dimension(filtration):
     simplices_by_dim = defaultdict(list)
     simplices_by_dim_only_filt = defaultdict(list)
-    for (simplex, filt_value) in filtration:
+    for simplex, filt_value in filtration:
         dim = len(simplex) - 1
         simplices_by_dim[dim].append(tuple(sorted(simplex)))
         simplices_by_dim_only_filt[dim].append(filt_value)
     return simplices_by_dim, simplices_by_dim_only_filt
+
 
 # For now, this is slow. Probably want to move this to the Rust component, and pass the simplices directly at some point.
 def compute_boundary_matrices(simplices):
@@ -40,7 +42,7 @@ def compute_boundary_matrices(simplices):
         lower_simplices = simplices.get(dim - 1, [])
         lower_index_map = index_maps.get(dim - 1, {})
 
-        rows = [] 
+        rows = []
         cols = []
         data = []
         # for each of the higher simplices
@@ -48,33 +50,40 @@ def compute_boundary_matrices(simplices):
             # for each of the vertices of the simplex
             for i, _ in enumerate(sorted(simplex)):
                 # get face missing this index
-                face = tuple(sorted(sorted(simplex)[:i] + sorted(simplex)[i+1:]))
+                face = tuple(simplex[:i] + simplex[i + 1 :])
                 # since this is a simplicial complex, the face is in the lower simplices
                 row = lower_index_map.get(face)
                 rows.append(row)
                 cols.append(col)
                 data.append((-1) ** i)
-        
+
         # create sparse matrix
         n_rows = len(lower_simplices)
         n_cols = len(higher_simplices)
-        boundary_matrices[dim] = {"n_rows": n_rows, "n_cols": n_cols, "data": np.asarray(data, dtype=np.float64), "rows": np.asarray(rows, dtype=np.int64), "cols": np.asarray(cols, dtype=np.int64)}
+        boundary_matrices[dim] = {
+            "n_rows": n_rows,
+            "n_cols": n_cols,
+            "data": np.asarray(data, dtype=np.float64),
+            "rows": np.asarray(rows, dtype=np.int64),
+            "cols": np.asarray(cols, dtype=np.int64),
+        }
 
     return boundary_matrices
+
 
 def filtration_hash_map(filtration, simplices_by_dim_only_filt):
     def max_index(lst, b):
         # Find the insertion point for b to the right
         idx = bisect.bisect_right(lst, b)
-        return idx 
+        return idx
+
     unique_filtration_values = sorted(list(set([f for (_, f) in filtration])))
-    print(unique_filtration_values)
     # For each filtration value, get indices for the boundary map at that filtration value
-    boundary_maps_index_dict = {filt_index: 
-                                    {  
-                                        key : max_index(filt_values, filt_value)
-                                        for key, filt_values in simplices_by_dim_only_filt.items()
-                                    } 
-                                    for (filt_index, filt_value) in enumerate(unique_filtration_values)
-                                }
+    boundary_maps_index_dict = {
+        filt_index: {
+            key: max_index(filt_values, filt_value)
+            for key, filt_values in simplices_by_dim_only_filt.items()
+        }
+        for (filt_index, filt_value) in enumerate(unique_filtration_values)
+    }
     return boundary_maps_index_dict
