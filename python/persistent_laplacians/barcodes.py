Fixed barcode computation for undying features

python/persistent_laplacians/barcodes.py
@@ -1,3 +1,4 @@
+import numpy as np
 def compute_barcodes(persistence_data, unique_filtration_values):
     max_filt_index = len(unique_filtration_values) - 1
     barcodes = {}
@@ -12,6 +13,6 @@ def compute_barcodes(persistence_data, unique_filtration_values):
         for i in range(len(unique_filtration_values)):
             persistent_i_inf = bettis.get((i, max_filt_index),0) - bettis.get((i-1, max_filt_index),0)
             if persistent_i_inf != 0:
-                barcodes_n[(i,-1)] = persistent_i_inf
+                barcodes_n[(i,np.inf)] = persistent_i_inf
         barcodes[n] = barcodes_n
     return barcodes

python/tests/test_homology.py
@@ -2,6 +2,7 @@ from persistent_laplacians.data import sphere_data
 from persistent_laplacians.barcodes import compute_barcodes
 from persistent_laplacians import persistent_laplacians as pl
 import pytest
+import numpy as np
 
 # generate a variety of sphere parameters
 ns = [20, 30, 40, 50]
@@ -32,10 +33,10 @@ def test_rust_vs_gudhi(n, d, r, noise, seed):
     barcodes = compute_barcodes(result, unique_filtration_values)
     print(barcodes)
     barcodes = [
-        (q, (unique_filtration_values[i], unique_filtration_values[j]))
+        (q, (unique_filtration_values[i], unique_filtration_values[j])) if j != np.inf else (q, (unique_filtration_values[i],np.inf))
         for q in barcodes.keys()
         for (i, j) in barcodes[q].keys()
-    ]
+    ]    
     sorted(barcodes)
 
     # TODO: somehow repeats for now in our code, so remove